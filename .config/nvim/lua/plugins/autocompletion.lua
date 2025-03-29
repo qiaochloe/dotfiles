f1f8e20e0f18180b8e7758ec611069c271d51533
Chloe Qiao <qiaochloe@gmail.com>
2025-03-28 20:56:57 -0400

    Add multicursor

--- .config/nvim/lua/plugins/autocompletion.lua
+++ .config/nvim/lua/plugins/autocompletion.lua
@@ -14,33 +14,31 @@ return {
       opts.mapping = vim.tbl_extend("force", opts.mapping, {
         -- Disable autocmpletion on enter
         ["<CR>"] = cmp.config.disable,
-        --[[
-        ["<Tab>"] = cmp.mapping(function(fallback)
-          if cmp.visible() then
-            -- You could replace select_next_item() with confirm({ select = true }) to get VS Code autocompletion behavior
-            cmp.select_next_item()
-          elseif vim.snippet.active({ direction = 1 }) then
-            vim.schedule(function()
-              vim.snippet.jump(1)
-            end)
-          elseif has_words_before() then
-            cmp.complete()
-          else
-            fallback()
-          end
-        end, { "i", "s" }),
-        ["<S-Tab>"] = cmp.mapping(function(fallback)
-          if cmp.visible() then
-            cmp.select_prev_item()
-          elseif vim.snippet.active({ direction = -1 }) then
-            vim.schedule(function()
-              vim.snippet.jump(-1)
-            end)
-          else
-            fallback()
-          end
-        end, { "i", "s" }),
-        --]]
+        --     ["<Tab>"] = cmp.mapping(function(fallback)
+        --       if cmp.visible() then
+        --         -- You could replace select_next_item() with confirm({ select = true }) to get VS Code autocompletion behavior
+        --         cmp.select_next_item()
+        --       elseif vim.snippet.active({ direction = 1 }) then
+        --         vim.schedule(function()
+        --           vim.snippet.jump(1)
+        --         end)
+        --       elseif has_words_before() then
+        --         cmp.complete()
+        --       else
+        --         fallback()
+        --       end
+        --     end, { "i", "s" }),
+        --     ["<S-Tab>"] = cmp.mapping(function(fallback)
+        --       if cmp.visible() then
+        --         cmp.select_prev_item()
+        --       elseif vim.snippet.active({ direction = -1 }) then
+        --         vim.schedule(function()
+        --           vim.snippet.jump(-1)
+        --         end)
+        --       else
+        --         fallback()
+        --       end
+        --     end, { "i", "s" }),
       })
     end,
   },
@@ -56,35 +54,6 @@ return {
         },
       },
     },
-  },
-  { -- Tabout
-    -- Used a recipe to use neotab with nvim-cmp
-    "kawre/neotab.nvim",
-    event = "InsertEnter",
-    opts = {
-      -- tabkey = "",
-    },
-  },
-  -- { -- Tabout
-  --   "abecodes/tabout.nvim",
-  --   lazy = false,
-  --   event = "InsertCharPre",
-  --   config = true,
-  --   opts = {
-  --     tabouts = {
-  --       { open = "'", close = "'" },
-  --       { open = '"', close = '"' },
-  --       { open = "`", close = "`" },
-  --       { open = "(", close = ")" },
-  --       { open = "[", close = "]" },
-  --       { open = "{", close = "}" },
-  --       { open = "$", close = "$" },
-  --     },
-  --   },
-  -- },
-  { -- Prevent copilot from showing suggestions in the completion menu
-    "zbirenbaum/copilot-cmp",
-    enabled = false,
   },
   { -- Remove snippets
     "garymjr/nvim-snippets",
--- .config/nvim/lua/plugins/custom.lua
+++ .config/nvim/lua/plugins/custom.lua
@@ -1,4 +1,68 @@
 return {
+  {
+    "jake-stewart/multicursor.nvim",
+    branch = "1.0",
+    config = function()
+      local mc = require("multicursor-nvim")
+      mc.setup()
+
+      local set = vim.keymap.set
+
+      -- Add a new cursor by matching word/selection
+      set({ "n", "x" }, "<leader>n", function()
+        mc.matchAddCursor(1)
+      end)
+      set({ "n", "x" }, "<leader>N", function()
+        mc.matchAddCursor(-1)
+      end)
+
+      -- Add a cursor for all matches of cursor word/selection in the document.
+      set({ "n", "x" }, "<leader>A", mc.matchAllAddCursors)
+
+      -- Append/insert for each line of visual selections.
+      -- Similar to block selection insertion.
+      set("x", "I", mc.insertVisual)
+      set("x", "A", mc.appendVisual)
+
+      -- Add and remove cursors with control + left click.
+      set("n", "<c-leftmouse>", mc.handleMouse)
+      set("n", "<c-leftdrag>", mc.handleMouseDrag)
+      set("n", "<c-leftrelease>", mc.handleMouseRelease)
+
+      -- Disable and enable cursors.
+      set({ "n", "x" }, "<c-q>", mc.toggleCursor)
+
+      -- Mappings defined in a keymap layer only apply when there are
+      -- multiple cursors. This lets you have overlapping mappings.
+      mc.addKeymapLayer(function(layerSet)
+        -- Select a different cursor as the main one.
+        layerSet({ "n", "x" }, "<left>", mc.prevCursor)
+        layerSet({ "n", "x" }, "<right>", mc.nextCursor)
+
+        -- Delete the main cursor.
+        layerSet({ "n", "x" }, "<leader>x", mc.deleteCursor)
+
+        -- Enable and clear cursors using escape.
+        layerSet("n", "<esc>", function()
+          if not mc.cursorsEnabled() then
+            mc.enableCursors()
+          else
+            mc.clearCursors()
+          end
+        end)
+      end)
+
+      -- Customize how cursors look.
+      local hl = vim.api.nvim_set_hl
+      hl(0, "MultiCursorCursor", { link = "Cursor" })
+      hl(0, "MultiCursorVisual", { link = "Visual" })
+      hl(0, "MultiCursorSign", { link = "SignColumn" })
+      hl(0, "MultiCursorMatchPreview", { link = "Search" })
+      hl(0, "MultiCursorDisabledCursor", { link = "Visual" })
+      hl(0, "MultiCursorDisabledVisual", { link = "Visual" })
+      hl(0, "MultiCursorDisabledSign", { link = "SignColumn" })
+    end,
+  },
   { -- Autopair parens, brackets, quotes, etc
     "windwp/nvim-autopairs",
     event = "InsertEnter",
@@ -29,6 +93,20 @@ return {
       })
     end,
   },
+  { -- After yank, the cursor does not move back to start
+    "svban/YankAssassin.nvim",
+    config = function()
+      require("YankAssassin").setup({
+        auto_normal = true, -- if true, autocmds are used. Whenever y is used in normal mode, the cursor doesn't move to start
+        auto_visual = true, -- if true, autocmds are used. Whenever y is used in visual mode, the cursor doesn't move to start
+      })
+    end,
+  },
+  { -- Splitting manager that works with wezterm
+    -- Keymaps are in `lua/config/keymaps.lua`
+    "mrjones2014/smart-splits.nvim",
+    lazy = false,
+  },
   { -- More languages to treesitter
     -- And disable syntax highlighting for LaTeX
     "nvim-treesitter/nvim-treesitter",
@@ -43,19 +121,5 @@ return {
         additional_vim_regex_highlighting = { "latex" },
       })
     end,
-  },
-  { -- After yank, the cursor does not move back to start
-    "svban/YankAssassin.nvim",
-    config = function()
-      require("YankAssassin").setup({
-        auto_normal = true, -- if true, autocmds are used. Whenever y is used in normal mode, the cursor doesn't move to start
-        auto_visual = true, -- if true, autocmds are used. Whenever y is used in visual mode, the cursor doesn't move to start
-      })
-    end,
-  },
-  { -- Splitting manager that works with wezterm
-    -- Keymaps are in `lua/config/keymaps.lua`
-    "mrjones2014/smart-splits.nvim",
-    lazy = false,
   },
 }
